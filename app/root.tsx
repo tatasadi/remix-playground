f0182074e8de99ac8b2ca98723595a6630bd299d
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-07-05 08:41:24 +0200

    add fonts and favicon

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,29 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import './tailwind.css'
+import { LinksFunction } from '@remix-run/node'
+import './styles/font.css'
 
+export const links: LinksFunction = () => {
+	return [{ rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }]
+}
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	)
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />
 }
